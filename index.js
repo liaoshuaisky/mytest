d346c1392e9401f261e1937c8e10da37c43233bc
liaoshuaisky <513283909@qq.com>
2019-11-15 11:47:00 +0800

    改变了index.js文件

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
-console.disableYellowBox = true;    //在模拟器中禁止弹出警告
+console.disableYellowBox = true;                  //在模拟器中禁止弹出警告12
 AppRegistry.registerComponent('myPro', () => App);
